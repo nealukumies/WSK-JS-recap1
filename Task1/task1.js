0935e9c3769be577e658ba5fbafc55e69199a477
nealukumies <nea.lukumies@metropolia.fi>
2025-03-18 11:11:29 +0200

    formatting task1, task 4

--- Task1/task1.js
+++ Task1/task1.js
@@ -1,10 +1,11 @@
-const input = prompt('Enter temperature in Celsius: ');
+input = prompt('Enter temperature in Celsius: ');
 const temperature = parseFloat(input.replace(',', '.'));
-if (!isNaN(temperature)){
-  const fahrenheit = (((9/5)*temperature) + 32).toFixed(2);
-  const p = document.createElement("p");
+if (!isNaN(temperature)) {
+  const fahrenheit = ((9 / 5) * temperature + 32).toFixed(2);
+  const p = document.createElement('p');
   document.body.appendChild(p);
-  document.querySelector("p").innerHTML = temperature + " C is " + fahrenheit + " F.";
-  } else {
-    alert("Invalid input, please enter a valid number!")
-  }
+  document.querySelector('p').innerHTML =
+    temperature + ' C is ' + fahrenheit + ' F.';
+} else {
+  alert('Invalid input, please enter a valid number!');
+}
--- Task4/task4.js
+++ Task4/task4.js
@@ -1,29 +1,31 @@
-const score = prompt("Enter your score:");
+const score = prompt('Enter your score:');
 let grade;
-const p = document.createElement("p");
+const p = document.createElement('p');
 document.body.appendChild(p);
-if (!isNaN(score)){
+if (!isNaN(score)) {
   if (score > 0 && score <= 39) {
     grade = 0;
-} else if (score >= 40 && score <= 51) {
+  } else if (score >= 40 && score <= 51) {
     grade = 1;
-} else if (score >= 52 && score <= 63) {
+  } else if (score >= 52 && score <= 63) {
     grade = 2;
-} else if (score >= 64 && score <= 75) {
+  } else if (score >= 64 && score <= 75) {
     grade = 3;
-} else if (score >= 76 && score <= 87) {
+  } else if (score >= 76 && score <= 87) {
     grade = 4;
-} else if (score >= 88 && score <= 100) {
+  } else if (score >= 88 && score <= 100) {
     grade = 5;
-} else {
-    grade = 6;
-}
-  if (grade < 6){
-    p.innerHTML = "With a score of " + score +", your grade is " + grade + ".";
   } else {
-    p.innerHTML = "Your score input (" + score + ") does not match possible score of 0-100."
+    grade = 6;
   }
-
+  if (grade < 6) {
+    p.innerHTML = 'With a score of ' + score + ', your grade is ' + grade + '.';
+  } else {
+    p.innerHTML =
+      'Your score input (' +
+      score +
+      ') does not match possible score of 0-100.';
+  }
 } else {
-  alert("Invalid input, try again!")
+  alert('Invalid input, try again!');
 }
