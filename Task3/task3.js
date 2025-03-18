7f319d2a54fda9f181e4c2e6b51ad92b50b23a67
nealukumies <nea.lukumies@metropolia.fi>
2025-03-18 09:39:27 +0200

    task3 js

--- Task3/task3.js
+++ Task3/task3.js
@@ -1,6 +1,24 @@
-function greeting ()
-{
-  console.log("Hello World!");
-};
+const a = prompt("Enter first side of triangle:");
+const b = prompt("Enter second side of triangle:");
+const c = prompt("Enter third side of triangle:");
 
-greeting();
+if (!isNaN(a || b || c)){
+  const p = document.createElement("p");
+  document.body.appendChild(p);
+  p.innerHTML = "The sides of your triangle: " + a + ", " + b + " and " + c;
+
+  const p2 = document.createElement("p");
+  document.body.appendChild(p2);
+  let triangle;
+  if (a == b && b == c){
+    triangle = "equilateral"
+  } else if (a != b && b != c){
+    triangle = "scalene"
+  } else {
+    triangle = "isosceles"
+  }
+  p2.innerHTML = "Triangle is " + triangle;
+
+} else {
+  alert("Invalid input, try again!");
+}
